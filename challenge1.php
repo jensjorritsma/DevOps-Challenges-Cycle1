bf5508e2285b3340fa43287e2b4950e49ff0b317
Jens Jorritsma <jens.jorritsma@rackspace.com>
2013-12-31 09:41:18 -0600

    General framework from documentation

--- challenge1.php
+++ challenge1.php
@@ -1,5 +1,45 @@
 #!/usr/bin/php
 
 <?php
-#More to come
+
+require 'vendor/autoload.php';
+
+$server = $compute->server();
+
+try {
+    $response = $server->create(array(
+        'name'     => 'Buildtest1',
+        'image'    => $ubuntu,
+        'flavor'   => $twoGbFlavor,
+    ));
+} catch (\Guzzle\Http\Exception\BadResponseException $e) {
+
+    // No! Something failed. Let's find out:
+
+    $responseBody = (string) $e->getResponse()->getBody();
+    $statusCode   = $e->getResponse()->getStatusCode();
+    $headers      = $e->getResponse()->getHeaderLines();
+
+    echo sprintf('Status: %s\nBody: %s\nHeaders: %s', $statusCode, $responseBody, implode(', ', $headers);
+}
+
+use OpenCloud\Compute\Constants\ServerState;
+
+$callback = function($server) {
+    if (!empty($server->error)) {
+        var_dump($server->error);
+        exit;
+    } else {
+        echo sprintf(
+            "Waiting on %s/%-12s %4s%%",
+            $server->name(),
+            $server->status(),
+            isset($server->progress) ? $server->progress : 0
+        );
+    }
+};
+
+$server->waitFor(ServerState::ACTIVE, 600, $callback);
+
+
 ?>
